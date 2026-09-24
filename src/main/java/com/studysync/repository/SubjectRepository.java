b126f5c5036ef32537e4e8547fd4e64254b41a09
Ajay vasala <vasalaajay36@gmail.com>
2026-09-24 20:20:00 +0530

    fix: support student-owned subject cleanup

--- src/main/java/com/studysync/repository/SubjectRepository.java
+++ src/main/java/com/studysync/repository/SubjectRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
-
     List<Subject> findByStudentId(Long studentId);
+    void deleteByStudentId(Long studentId);
 }
