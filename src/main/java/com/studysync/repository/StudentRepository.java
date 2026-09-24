86ba4c864ea59ea8c73a0aee5913f595cc473ba1
Ajay vasala <vasalaajay36@gmail.com>
2026-09-24 20:50:58 +0530

    feat: resolve students by email for session login

--- src/main/java/com/studysync/repository/StudentRepository.java
+++ src/main/java/com/studysync/repository/StudentRepository.java
@@ -3,6 +3,8 @@ package com.studysync.repository;
 import com.studysync.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudentRepository extends JpaRepository<Student, Long> {
+import java.util.Optional;
 
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findByEmailIgnoreCase(String email);
 }
